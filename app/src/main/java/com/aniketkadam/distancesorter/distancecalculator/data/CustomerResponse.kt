37372238c701d782061378c6622985a246c38ce1
Aniket Kadam <anikadamg@gmail.com>
2019-10-31 17:34:25 +0530

    Made CustomerResponse fields private to better express class intentions This is only supposed to be used as a mapper to Customer, not leaving it open to be used directly.

--- app/src/main/java/com/aniketkadam/distancesorter/distancecalculator/data/CustomerResponse.kt
+++ app/src/main/java/com/aniketkadam/distancesorter/distancecalculator/data/CustomerResponse.kt
@@ -3,10 +3,10 @@ package com.aniketkadam.distancesorter.distancecalculator.data
 import com.google.gson.annotations.SerializedName
 
 data class CustomerResponse(
-    @SerializedName("latitude") val latitude: Double,
-    @SerializedName("longitude") val longitude: Double,
-    @SerializedName("user_id") val userId: Int,
-    @SerializedName("name") val name: String
+    @SerializedName("latitude") private val latitude: Double,
+    @SerializedName("longitude") private val longitude: Double,
+    @SerializedName("user_id") private val userId: Int,
+    @SerializedName("name") private val name: String
 ) {
     fun toCustomer(): Customer = Customer(Coordinates(latitude, longitude), userId, name)
 }
