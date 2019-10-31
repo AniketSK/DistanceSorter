255bbf9f7923e58e4e78affbd6c11dc548387fcc
Aniket Kadam <anikadamg@gmail.com>
2019-10-31 18:05:09 +0530

    Added few more required params to the repo.

--- app/src/main/java/com/aniketkadam/distancesorter/distancecalculator/DistanceViewModelContract.kt
+++ app/src/main/java/com/aniketkadam/distancesorter/distancecalculator/DistanceViewModelContract.kt
@@ -1,9 +1,12 @@
 package com.aniketkadam.distancesorter.distancecalculator
 
+import com.aniketkadam.distancesorter.distancecalculator.data.Coordinates
 import com.aniketkadam.distancesorter.distancecalculator.data.Customer
 
 interface DistanceViewModelContract {
     interface Repository {
         fun getAllCustomers(): List<Customer>
+        fun getCoordinatesToMeasureDistanceFrom(): Coordinates
+        fun getMinimumDistance(): Double
     }
 }
