661bc5ad64b00be31575629627101c8281cee3c2
Aniket Kadam <anikadamg@gmail.com>
2019-10-31 18:20:00 +0530

    Calculating the closest customers with the viewmodel.

--- app/src/main/java/com/aniketkadam/distancesorter/distancecalculator/DistanceCalculatorViewModel.kt
+++ app/src/main/java/com/aniketkadam/distancesorter/distancecalculator/DistanceCalculatorViewModel.kt
@@ -1,5 +1,18 @@
 package com.aniketkadam.distancesorter.distancecalculator
 
 import androidx.lifecycle.ViewModel
+import com.aniketkadam.distancesorter.distancecalculator.data.Customer
 
-class DistanceCalculatorViewModel : ViewModel()
+class DistanceCalculatorViewModel(private val repo: DistanceViewModelContract.Repository) :
+    ViewModel() {
+
+    private val withinMinimumDistanceUseCase = WithinMinimumDistanceUsecase()
+
+    fun getClosestCustomers(): List<Customer> {
+        val origin = repo.getCoordinatesToMeasureDistanceFrom()
+        val minimumDistance = repo.getMinimumDistance()
+
+        return withinMinimumDistanceUseCase.execute(origin, minimumDistance, repo.getAllCustomers())
+    }
+
+}
