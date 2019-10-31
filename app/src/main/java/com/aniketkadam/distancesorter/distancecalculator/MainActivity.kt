b17fd1c03ba9843c379db1012fdcb6aff2c0b976
Aniket Kadam <anikadamg@gmail.com>
2019-10-31 17:09:54 +0530

    Moved the MainActivity to a relevant package.

--- app/src/main/java/com/aniketkadam/distancesorter/distancecalculator/MainActivity.kt
+++ app/src/main/java/com/aniketkadam/distancesorter/distancecalculator/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.aniketkadam.distancesorter
+package com.aniketkadam.distancesorter.distancecalculator
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.aniketkadam.distancesorter.R
 
 class MainActivity : AppCompatActivity() {
 
