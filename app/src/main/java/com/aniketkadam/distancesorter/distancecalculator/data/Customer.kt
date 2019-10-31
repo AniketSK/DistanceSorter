e9f4e094f9f634fc6fe6ebbca1cd5cda87d42681
Aniket Kadam <anikadamg@gmail.com>
2019-10-31 17:18:06 +0530

    Added a data class to represent customers.

---
+++ app/src/main/java/com/aniketkadam/distancesorter/distancecalculator/data/Customer.kt
@@ -0,0 +1,8 @@
+package com.aniketkadam.distancesorter.distancecalculator.data
+
+data class Customer(
+    val latitude: Double,
+    val longitude: Double,
+    val userId: Int,
+    val name: String
+)
