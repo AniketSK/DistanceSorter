3b9eb1d39c2cb7d328d5f5e069aefe98e93b13ce
Aniket Kadam <anikadamg@gmail.com>
2019-10-31 19:28:50 +0530

    Added rounding to error margins. The error in calculating earth's radius limits precision to 0.5%

--- app/src/main/java/com/aniketkadam/distancesorter/distancecalculator/WithinMinimumDistanceUsecase.kt
+++ app/src/main/java/com/aniketkadam/distancesorter/distancecalculator/WithinMinimumDistanceUsecase.kt
@@ -3,6 +3,8 @@ package com.aniketkadam.distancesorter.distancecalculator
 import androidx.annotation.VisibleForTesting
 import com.aniketkadam.distancesorter.distancecalculator.data.Coordinates
 import com.aniketkadam.distancesorter.distancecalculator.data.Customer
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class WithinMinimumDistanceUsecase {
 
@@ -14,6 +16,12 @@ class WithinMinimumDistanceUsecase {
     @VisibleForTesting
     fun isWithinMinimumDistance(pointA: Coordinates, pointB: Coordinates, minimumDistance: Double) =
         calculateDistance(pointA, pointB) <= minimumDistance
+
+    @VisibleForTesting
+    fun roundToErrorMargin(number: Double) = BigDecimal(number).setScale(
+        1,
+        RoundingMode.HALF_EVEN
+    ) // Error margin is 0.5km for a 100km radius
 
     fun execute(origin: Coordinates, minimumDistance: Double, customers: List<Customer>) =
         customers.filter { isWithinMinimumDistance(origin, it.location, minimumDistance) }
