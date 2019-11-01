768645c24e32a54534db0d5b5fb606d7f2481da3
Aniket Kadam <anikadamg@gmail.com>
2019-11-01 13:00:42 +0530

    Created a view holder for the customer information

---
+++ app/src/main/java/com/aniketkadam/distancesorter/distancecalculator/CustomerViewHolder.kt
@@ -0,0 +1,6 @@
+package com.aniketkadam.distancesorter.distancecalculator
+
+import androidx.recyclerview.widget.RecyclerView
+import com.aniketkadam.distancesorter.databinding.CustomerDisplayBinding
+
+class CustomerViewHolder(view: CustomerDisplayBinding) : RecyclerView.ViewHolder(view.root)
