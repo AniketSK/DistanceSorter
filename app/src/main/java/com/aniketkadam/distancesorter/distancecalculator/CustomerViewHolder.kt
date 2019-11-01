bb0e3c27a72553abdc0a2c6df5e687baa439b922
Aniket Kadam <anikadamg@gmail.com>
2019-11-01 13:08:54 +0530

    Bound data into the viewholder.

--- app/src/main/java/com/aniketkadam/distancesorter/distancecalculator/CustomerViewHolder.kt
+++ app/src/main/java/com/aniketkadam/distancesorter/distancecalculator/CustomerViewHolder.kt
@@ -2,5 +2,15 @@ package com.aniketkadam.distancesorter.distancecalculator
 
 import androidx.recyclerview.widget.RecyclerView
 import com.aniketkadam.distancesorter.databinding.CustomerDisplayBinding
+import com.aniketkadam.distancesorter.distancecalculator.data.Customer
 
-class CustomerViewHolder(view: CustomerDisplayBinding) : RecyclerView.ViewHolder(view.root)
+class CustomerViewHolder(private val binding: CustomerDisplayBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(customerData: Customer) {
+        binding.apply {
+            customer = customerData
+            executePendingBindings()
+        }
+    }
+}
