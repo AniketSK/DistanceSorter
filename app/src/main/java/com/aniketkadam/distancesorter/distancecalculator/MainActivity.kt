6dfeb432b7c53781f0ed96fe5bdcbdd1dea9a987
Aniket Kadam <anikadamg@gmail.com>
2019-11-01 13:42:02 +0530

    Connected the vm's state to a UI render function. Hiding the load indicator when the loading's done. Adding new information to the adapter when received.

--- app/src/main/java/com/aniketkadam/distancesorter/distancecalculator/MainActivity.kt
+++ app/src/main/java/com/aniketkadam/distancesorter/distancecalculator/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.aniketkadam.distancesorter.distancecalculator
 
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
 import com.aniketkadam.distancesorter.R
 import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -17,10 +19,22 @@ class MainActivity : DaggerAppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         initRecyclerView()
+        vm.customersWithinMinDistance.observe(this, Observer {
+            renderData(it)
+        })
     }
 
     private fun initRecyclerView() {
         customerList.adapter = adapter
     }
 
+    private fun renderData(it: Lce?): Unit = when (it) {
+        is Lce.Loading -> emptyView.visibility = View.VISIBLE
+        is Lce.Content -> {
+            emptyView.visibility = View.GONE
+            adapter.submitList(it.customers)
+        }
+        null -> {
+        }
+    }
 }
