732c8e9f06a75efe73b60ac94fc031aa48e5f7d2
Aniket Kadam <anikadamg@gmail.com>
2019-11-01 13:40:45 +0530

    Initializing the recyclerview

--- app/src/main/java/com/aniketkadam/distancesorter/distancecalculator/MainActivity.kt
+++ app/src/main/java/com/aniketkadam/distancesorter/distancecalculator/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.aniketkadam.distancesorter.distancecalculator
 
 import android.os.Bundle
-import android.util.Log
-import androidx.lifecycle.Observer
 import com.aniketkadam.distancesorter.R
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 class MainActivity : DaggerAppCompatActivity() {
 
     @Inject
     lateinit var vm: DistanceCalculatorViewModel
+    val adapter: CustomerAdapter = CustomerAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initRecyclerView()
     }
+
+    private fun initRecyclerView() {
+        customerList.adapter = adapter
+    }
+
 }
