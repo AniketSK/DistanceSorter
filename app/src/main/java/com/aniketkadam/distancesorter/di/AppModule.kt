68fc3bc831b5a3d2fc67f7b23d62817705affb8b
Aniket Kadam <anikadamg@gmail.com>
2019-11-01 12:47:41 +0530

    Fixed AppModule not being able to provide a context Since only daggerapplication was being injected it didn't know how to convert

--- app/src/main/java/com/aniketkadam/distancesorter/di/AppModule.kt
+++ app/src/main/java/com/aniketkadam/distancesorter/di/AppModule.kt
@@ -1,13 +1,13 @@
 package com.aniketkadam.distancesorter.di
 
-import android.app.Application
 import android.content.Context
 import dagger.Binds
 import dagger.Module
+import dagger.android.support.DaggerApplication
 
 @Module
 abstract class AppModule {
     @Binds
-    abstract fun provideContext(application: Application): Context
+    abstract fun provideContext(application: DaggerApplication): Context
 
 }
