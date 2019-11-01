9d140e7221421f653d8d45c2cec4353a963071d7
Aniket Kadam <anikadamg@gmail.com>
2019-11-01 12:49:18 +0530

    Implemented the component in the Application

--- app/src/main/java/com/aniketkadam/distancesorter/distancecalculator/DistanceSorterApplication.kt
+++ app/src/main/java/com/aniketkadam/distancesorter/distancecalculator/DistanceSorterApplication.kt
@@ -1,5 +1,15 @@
 package com.aniketkadam.distancesorter.distancecalculator
 
-import android.app.Application
+import com.aniketkadam.distancesorter.di.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.support.DaggerApplication
 
-class DistanceSorterApplication : Application()
+class DistanceSorterApplication : DaggerApplication() {
+
+    val appComponent by lazy {
+        DaggerAppComponent.factory().create(this)
+    }
+
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> = appComponent
+
+}
